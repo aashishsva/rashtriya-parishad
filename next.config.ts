e778da4d6a8b3cb2d8aa01246c5b8311829bdc72
aashishsva <aashish.sva@gmail.com>
2026-03-27 09:57:16 +0530

    config updated

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["webdemo.cyrusrecharge.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "webdemo.cyrusrecharge.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
